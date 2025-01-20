7ecffdcd0b463a78ac2ec625b6595311c0866af0
samudev <sambea2103@gmail.com>
2025-01-20 13:43:52 -0500

    feat: add linter w/ biome

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander'
+import { Command } from 'commander';
 
 // Declare the program
 const program = new Command();
 
 // Add actions
 program
-.argument('<string>', 'string to log')
-.action((message: string) => {console.log('Hello World ' + message)})
-.description('Log a message')
+	.argument('<string>', 'string to log')
+	.action((message: string) => {
+		console.log(`Hello World ${message}`);
+	})
+	.description('Log a message');
 
 // Execute the CLI
-program.parse(process.argv)
+program.parse(process.argv);
